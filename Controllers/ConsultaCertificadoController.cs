0d09e5797946b16a06e66539d1aad9fedce6a043
Juan Carlos Bonilla González <jcarlosbonilla.gonzalez@gmail.com>
2024-08-28 21:42:03 -0500

    Ajuste del index y formulario principal de consulta

---
+++ Controllers/ConsultaCertificadoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportesCabildoAwa.Controllers
+{
+    public class ConsultaCertificadoController : Controller
+    {
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+    }
+
+}
